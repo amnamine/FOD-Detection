5a8fa861425c8491c4ddc1e28fe2a82f7e7792e8
Amn Amine <105323588+amnamine@users.noreply.github.com>
2025-05-17 12:27:31 +0100

    Update anayse.py

--- anayse.py
+++ anayse.py
@@ -1,7 +1,7 @@
 from ultralytics import YOLO
 
 # Load the model using YOLO
-model = YOLO('fod.pt')
+model = YOLO('fod50.pt')
 
 # Print number of classes
 print(f"Number of classes in FOD dataset: {len(model.names)}")
